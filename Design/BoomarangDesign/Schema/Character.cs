dab3f1f6245a03760a2513ad7d29338cfc6facbc
Steve <Steve.mccann@live.com>
2018-02-23 14:07:01 -0500

    added character Type

--- Design/BoomarangDesign/Schema/Character.cs
+++ Design/BoomarangDesign/Schema/Character.cs
@@ -11,5 +11,9 @@ namespace Schema
         public CharacterName Name { get; set; }
         public List<string> Stories; // what stories this character is in 
         List<String> Bullets { get; set; } // some bullet points we may want to display 
+        List<Realivies> Family { get; set; } // list of family 
+        public CharacterType Type { get; set; }
+                
     }
+    public enum CharacterType { Lead,Major,Minor,OffScreen}
 }
