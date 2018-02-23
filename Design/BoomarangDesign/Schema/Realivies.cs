19a7840d2f3db999db2275d3cd729a88c87ce1f0
Steve <Steve.mccann@live.com>
2018-02-23 14:07:34 -0500

    added type and retaliative

---
+++ Design/BoomarangDesign/Schema/Realivies.cs
@@ -0,0 +1,9 @@
+﻿namespace Schema
+{
+    public class Realivies
+    {
+        public Character Person { get; set; }
+        public Relation Relation { get; set; }
+    }
+    public enum Relation { Mother, Father, Sister, Brother, Son, Daughter }
+}
