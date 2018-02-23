68693afe13690bfc595a8c23010e3b0c2ac0bfa7
Steve <Steve.mccann@live.com>
2018-02-23 11:48:37 -0500

    checked in

---
+++ Design/BoomarangDesign/Schema/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schema
+{
+    public class Character
+    {
+        public string ID { get; set; }
+        public string DisplayName { get; set; } // the name used to identify the character on most screens 
+        public CharacterName Name { get; set; }
+        public List<string> Stories; // what stories this character is in 
+        List<String> Bullets { get; set; } // some bullet points we may want to display 
+    }
+}
